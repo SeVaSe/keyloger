d414c47be52e658a0a8062d53f9d1d4e9a8fc203
DelZeiT <maksimcvetkov1106@gmail.com>
2023-08-15 21:44:42 +0300

    TIC-004: добавил скрытие программы из списка активных задач.

--- hide_programm.py
+++ hide_programm.py
@@ -1,1 +1,9 @@
 '''ФАЙЛ МАКСИМА, СКРЫТЬ ПРОГРАММУ'''
+import win32gui
+import win32con
+
+# получение дескриптора текущего окна
+hwnd = win32gui.GetForegroundWindow()
+
+# скрытие окно из списка активных задач
+win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
